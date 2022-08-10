ea0510a6aa256cefefa2aa0eb3d82af5904c6f43
Brian <brian@mrmicropc.com>
2022-08-10 17:58:15 -0600

    Add manifest for AWS kebernetes

--- aws_docker_build.sh
+++ aws_docker_build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 // Connect docker to Amazon Repo
-sudo docker login -u AWS -p $(aws ect get-login-password --region-us-west-1) 037452191287.dkr.ecr.us-west-1.amazonaws.com
+sudo docker login -u AWS -p $(aws ecr get-login-password --region-us-west-1) 037452191287.dkr.ecr.us-west-1.amazonaws.com
 
 // Build docker image
 mkdir -p awsbuild
-cp ./Docker/* ./awsbuid/
+cp ./Docker/* ./awsbuild/
 cp ./Node/* ./awsbuild/
 
 docker build -t brian ./awsbuild
