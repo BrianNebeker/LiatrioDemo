d3413d7389b384c7271c561110df1d92c2e79dcd
Brian <brian@mrmicropc.com>
2022-08-17 22:08:07 -0600

    Fix replace build number

--- local_docker_build.sh
+++ local_docker_build.sh
@@ -2,5 +2,5 @@
 mkdir -p build
 cp ./Docker/* ./build/
 cp ./Node/* ./build/
-sed -i 's/%BUILD%/$1/g' ./build/script.js
+sed -i "s/%BUILD%/${1}/g" ./build/script.js
 docker build -t demo-azure:$1 ./build
