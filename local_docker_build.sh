db6d260d3c9660a48ad65b807672a28eee4d1072
Brian <brian@mrmicropc.com>
2022-08-17 21:49:12 -0600

    Fix docker image name

--- local_docker_build.sh
+++ local_docker_build.sh
@@ -3,4 +3,4 @@ mkdir -p build
 cp ./Docker/* ./build/
 cp ./Node/* ./build/
 sed -i 's/%BUILD%/$1/g' ./build/script.js
-docker build -t brian/liatrodemo:$1 ./build
+docker build -t demo-azure:$1 ./build
